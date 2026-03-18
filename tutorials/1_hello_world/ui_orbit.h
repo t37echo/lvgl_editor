b427463fe204dc8f04c02a6537326f60c9228375
t37echo <2tech37irobs@gmail.com>
2026-03-18 14:00:10 +0300

    init project code recreated

---
+++ tutorials/1_hello_world/ui_orbit.h
@@ -0,0 +1,45 @@
+/**
+ * @file ui_ORBIT.h
+ */
+
+#ifndef UI_ORBIT_H
+#define UI_ORBIT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Initialize the component library
+ */
+void ui_orbit_init(const char * asset_path);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*UI_ORBIT_H*/
